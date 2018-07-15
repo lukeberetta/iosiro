e0ad93c22ca06fa76cddf504592104b736697988
Luke Beretta <thelukeberetta@gmail.com>
2018-07-15 13:34:52 +0200

    mobile menu, links

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 // Custom Medium API
-
 const converter = "https://api.rss2json.com/v1/api.json?rss_url=";
 const mediumLink = "https%3A%2F%2Fmedium.com%2Ffeed%2F%40NickyWoolf%3Fformat%3Djson";
 const endpoint = converter + mediumLink;
@@ -24,17 +23,12 @@ function pullMedium(url) {
 function handleMedium(data) {
   let posts = data.items;
   let squares = ["#square1", "#square2", "#square3", "#square4"];
+
   for (var i = 0; i < squares.length; i++) {
     let title = posts[i].title;
     let link = posts[i].link;
-    var options = {
-      year: "numeric",
-      month: "short",
-      day: "numeric"
-    };
     let date = posts[i].pubDate;
     date = date.replace(/-/g, "/").replace(/ .*/, "");
-    // console.log(`\n Title: ${title} \n Link: ${link} \n Date: ${date}`);
     document.querySelector(squares[i]).innerHTML =
       `<a href="${link}" target="_blank" class="post" id="${squares[i]}">
         <h2>${title}</h2>
@@ -52,8 +46,8 @@ function toggleSideMenu() {
   let links = document.getElementById("side-links");
   let close = document.getElementById("btn-close");
 
-  if (menu.style.width !== "100vw") {
-    menu.style.width = "100vw";
+  if (menu.style.width !== "80vw") {
+    menu.style.width = "80vw";
     links.style.opacity = 1;
     close.style.opacity = .9;
   } else {
